f91be6612515e3b6190448b42e3d6907b103df96
xx <784457618@qq.com>
2023-12-04 04:30:05 +0800

    [update] add kafka

--- api/src/flash.rs
+++ api/src/flash.rs
@@ -6,3 +6,10 @@ pub struct Whortleberry<T> {
     pub err_msg: String,
     pub data: T,
 }
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct Task {
+    pub id: String,
+    pub name: String,
+    pub last_heartbeat: i64,
+}
--- api/src/lib.rs
+++ api/src/lib.rs
@@ -3,7 +3,7 @@ pub mod animal;
 pub mod flash;
 
 use axum::{
-    extract::State,
+    extract::{Query, State},
     http::{HeaderMap, Method},
     routing::{get, post},
     Json, Router,
@@ -12,7 +12,7 @@ use axum::{
 use ::chrono::Local;
 use axum::BoxError;
 use flash::Whortleberry;
-use log::{error, info};
+use log::{debug, error, info};
 use serde::{Deserialize, Serialize};
 use sqlx::{
     mysql::MySqlPoolOptions,
@@ -20,7 +20,7 @@ use sqlx::{
     MySql, Pool,
 };
 
-use std::{collections::HashMap, net::SocketAddr, time::Duration};
+use std::{collections::HashMap, net::SocketAddr, thread, time::Duration};
 
 use std::sync::{Arc, Mutex, MutexGuard};
 
@@ -31,8 +31,12 @@ use tower_http::{
     limit::RequestBodyLimitLayer,
 };
 
+use crate::flash::Task;
+
 lazy_static! {
     pub static ref W_LOCK: Arc<Mutex<i64>> = Arc::new(Mutex::new(0));
+    pub static ref GLOBAL_TASK_POOL: Arc<Mutex<HashMap<String, Task>>> =
+        Arc::new(Mutex::new(HashMap::new()));
 }
 
 pub async fn start(app_conf: conf::app::AppConfig) -> anyhow::Result<()> {
@@ -103,24 +107,49 @@ pub async fn time_out_handler(err: BoxError) -> Json<Whortleberry<HashMap<String
     return v;
 }
 
+#[derive(Debug, Serialize, Default, Deserialize)]
+pub struct NewTaskRequest {
+    pub task_id: String,
+}
 /// add background task
-async fn start_task(_state: State<AppState>) -> Json<Whortleberry<(String, String)>> {
-    tokio::task::spawn(async {
-        let mut v = W_LOCK.lock().unwrap();
-        *v += 1;
-        info!("start a new task....... {v}",);
-        let cry = service::task::json::ChrysaetosBit::new("_".to_owned(), 32);
-        let obj = serde_json::from_str(r###"{"foo":"baz","complex":[1,2,3]}"###).unwrap();
-        let res = cry.parse(&obj);
-        info!("res {:?}", serde_json::to_string(&res).unwrap());
+async fn start_task(
+    _state: State<AppState>,
+    query: Query<NewTaskRequest>,
+) -> Json<Whortleberry<(String, String, HashMap<String, Task>)>> {
+    info!("task id {:?}", query.task_id);
+    let task_id = Arc::new(query.task_id.clone());
+    let handler = tokio::task::spawn(async move {
+        let mut task_p: MutexGuard<'_, HashMap<String, Task>> = GLOBAL_TASK_POOL.lock().unwrap();
+
+        if (*task_p).contains_key(task_id.as_str()) {
+            info!("task is running {}", task_id);
+            return;
+        }
+        // insert task
+        tokio::task::spawn(async {});
+        task_p.insert(
+            task_id.clone().to_string(),
+            Task {
+                id: task_id.to_owned().to_string(),
+                name: "foo".to_owned(),
+                last_heartbeat: 0,
+            },
+        );
     });
+
     let _cnt: MutexGuard<'_, i64> = W_LOCK.lock().unwrap();
+    let v: MutexGuard<'_, HashMap<String, Task>> = GLOBAL_TASK_POOL.lock().unwrap();
+    let mut data: HashMap<String, Task> = HashMap::new();
+    for ele in v.iter() {
+        data.insert(ele.0.to_string(), ele.1.to_owned().clone());
+    }
     Json(Whortleberry {
         err_no: 10000,
         err_msg: format!("success",).to_owned(),
         data: (
             Local::now().format("%Y-%m-%d %H:%M:%S.%f").to_string(),
             format!("success {}", *_cnt + 1),
+            data,
         ),
     })
 }
@@ -181,6 +210,76 @@ async fn build_db(db: conf::app::DbConfig) -> Pool<MySql> {
                 "🔥 Failed to connect to the  database dsn {:?}: {:?}",
                 db.dsn, err
             );
+        }
+    }
+}
+
+use kafka::{
+    consumer::{Consumer, FetchOffset, GroupOffsetStorage, MessageSets},
+    producer::AsBytes,
+};
+
+async fn start_clean_task(task_id: String, group: String, topic: String, brokers: Vec<String>) {
+    // todo
+    {
+        let tasks = GLOBAL_TASK_POOL.lock().unwrap();
+        if (*tasks).contains_key(task_id.to_owned().as_str()) {
+            info!("task{} is already in pool", task_id);
+            let before = Duration::from_secs(
+                (*tasks)
+                    .get(task_id.to_owned().as_str())
+                    .unwrap()
+                    .last_heartbeat as u64,
+            );
+            if std::time::Instant::now().elapsed() - before < Duration::from_secs(60) {
+                return;
+            }
+        }
+    }
+    let mut conn = Consumer::from_hosts(brokers)
+        .with_topic(topic.to_owned())
+        .with_group(group.to_owned())
+        .with_fallback_offset(FetchOffset::Earliest)
+        .with_fetch_max_bytes_per_partition(1_000_000)
+        .with_offset_storage(Some(GroupOffsetStorage::Kafka))
+        .create()
+        .unwrap();
+    info!("链接 kafka 成功 {}", conn.client().client_id());
+
+    let mut cnt = 0;
+    loop {
+        let m = conn.poll();
+        if m.is_err() {
+            error!("poll {} msg error {:?}", topic, m.err());
+            thread::sleep(Duration::from_millis(500));
+
+            continue;
+        }
+
+        let msgs = m.unwrap();
+
+        if msgs.is_empty() {
+            debug!("empty msg {}", topic);
+            thread::sleep(Duration::from_millis(500));
+            continue;
+        }
+        for cmsg in msgs.iter() {
+            for msg in cmsg.messages() {
+                cnt += 1;
+                info!("msg {:?}", msg.value.as_bytes());
+            }
+        }
+        conn.commit_consumed().unwrap();
+        if cnt % 1000 == 0 {
+            {
+                let mut l = GLOBAL_TASK_POOL.lock().unwrap();
+                if (*l).contains_key(task_id.to_owned().as_str()) {
+                    let v: &Task = (*l).get(task_id.to_owned().as_str()).unwrap();
+                    let mut v = v.clone();
+                    v.last_heartbeat = 1;
+                    (*l).insert(task_id.to_owned(), v);
+                }
+            }
         }
     }
 }
