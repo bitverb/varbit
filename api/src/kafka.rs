3498ef42d9b98eb04f47118964d7baa4c60d01c9
xx <784457618@qq.com>
2023-12-14 15:31:04 +0800

    [update] remove unused

--- api/src/kafka.rs
+++ api/src/kafka.rs
@@ -1,98 +1,13 @@
 use std::time::Duration;
 
-use log::{info, warn};
-use rdkafka::client::ClientContext;
-use rdkafka::config::RDKafkaLogLevel;
+use log::info;
+
 use rdkafka::consumer::{
-    BaseConsumer, CommitMode, Consumer, ConsumerContext, Rebalance, StreamConsumer,
+    BaseConsumer, Consumer,
 };
-use rdkafka::error::KafkaResult;
-use rdkafka::message::{Headers, Message};
-use rdkafka::topic_partition_list::TopicPartitionList;
+
 use rdkafka::ClientConfig;
 use serde::Deserialize;
-struct CustomContext {
-    pub id: String,
-}
-
-impl ClientContext for CustomContext {}
-
-impl ConsumerContext for CustomContext {
-    fn pre_rebalance(&self, rebalance: &Rebalance) {
-        info!("Pre rebalance client{} {:?}", self.id.to_owned(), rebalance);
-    }
-
-    fn post_rebalance(&self, rebalance: &Rebalance) {
-        info!("Post rebalance {:?}", rebalance);
-    }
-
-    fn commit_callback(&self, result: KafkaResult<()>, _offsets: &TopicPartitionList) {
-        info!("Committing offsets: {:?}", result);
-    }
-}
-
-// A type alias with your custom consumer can be created for convenience.
-
-// A type alias with your custom consumer can be created for convenience.
-type LoggingConsumer = StreamConsumer<CustomContext>;
-
-pub async fn consume_and_print(brokers: &str, group_id: &str, topics: &[&str]) {
-    info!("ing...");
-    let context = CustomContext {
-        id: mongodb::bson::oid::ObjectId::new().to_hex(),
-    };
-    let id = context.id.clone();
-    let consumer: LoggingConsumer = ClientConfig::new()
-        .set("group.id", group_id)
-        .set("bootstrap.servers", brokers)
-        .set("enable.partition.eof", "false")
-        .set("session.timeout.ms", "6000")
-        .set("enable.auto.commit", "true")
-        //.set("statistics.interval.ms", "30000")
-        //.set("auto.offset.reset", "smallest")
-        .set_log_level(RDKafkaLogLevel::Debug)
-        .create_with_context(context)
-        .expect("Consumer creation failed");
-
-    consumer
-        .subscribe(&topics.to_vec())
-        .expect("Can't subscribe to specified topics");
-    let cry = service::task::json::ChrysaetosBit::new("debug".to_owned(), "_".to_owned(), 32);
-
-    loop {
-        match consumer.recv().await {
-            Err(e) => warn!("Kafka error: {}", e),
-            Ok(m) => {
-                let payload = match m.payload_view::<str>() {
-                    None => "",
-                    Some(Ok(s)) => s,
-                    Some(Err(e)) => {
-                        warn!("Error while deserializing message payload: {:?}", e);
-                        ""
-                    }
-                };
-                info!("key: '{:?}', payload: '{}', topic: {}, partition: {}, offset: {}, timestamp: {:?} {:?}",
-                      m.key(), payload, m.topic(), m.partition(), m.offset(), m.timestamp(),payload);
-
-                if let Some(headers) = m.headers() {
-                    for i in 0..headers.count() {
-                        let header = headers.get(i).unwrap();
-                        info!("  Header {:#?}: {:?}", header.0, header.1);
-                    }
-                }
-                // parser
-                let obj: serde_json::Value = serde_json::from_str(payload).unwrap();
-                let res = cry.parse(&"test".to_owned(), &obj);
-                info!(
-                    "id = {:?} res = {:?}",
-                    id,
-                    serde_json::to_string(&res).unwrap()
-                );
-                consumer.commit_message(&m, CommitMode::Async).unwrap();
-            }
-        };
-    }
-}
 
 pub fn kafka_test_connect(cfg: &serde_json::Value) -> anyhow::Result<(), String> {
     let kcc = match serde_json::from_value::<KafkaConnectReqCfg>(cfg.clone()) {
